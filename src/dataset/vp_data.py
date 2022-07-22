5715e91918d5ddb971340875a24dfbae159607eb
George Kouros <george.kouros.ece@gmail.com>
2022-07-22 11:11:05 +0200

    Add NeMo style dataset class

--- src/dataset/vp_data.py
+++ src/dataset/vp_data.py
@@ -7,9 +7,12 @@ import pandas as pd
 import torch
 import torchvision.transforms as transforms
 import torch.utils.data as data
+import cv2
 
-from .data_utils import process_viewpoint_label, TransLightning, resize_pad, random_crop
-
+try:
+    from .data_utils import process_viewpoint_label, TransLightning, resize_pad, random_crop
+except:
+    from data_utils import process_viewpoint_label, TransLightning, resize_pad, random_crop
 
 # ImageNet statistics
 imagenet_pca = {
@@ -19,11 +22,137 @@ imagenet_pca = {
                                 [-0.5836, -0.6948, 0.4203]])
 }
 
+categories = [
+            'aeroplane', 'bicycle', 'boat', 'bottle', 'bus', 'car', 'chair',
+            'diningtable', 'motorbike', 'sofa', 'train', 'tvmonitor']
+
+blacklist = {category: [] for category in categories}
+blacklist['motorbike'] += ['n03791053_8626.JPEG', 'n03791053_8438.JPEG']
 
 # Define normalization and random disturb for input image
 disturb = TransLightning(0.1, imagenet_pca['eigval'], imagenet_pca['eigvec'])
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
+class Pascal3D_NeMo(data.Dataset):
+    def __init__(self, root_dir, input_dim=224, offset=0, cls_choice=None, occlusion_level=0):
+        if occlusion_level > 0:
+            self.root_dir = os.path.join(root_dir, 'PASCAL3D_OCC_NeMo')
+            self.data_pendix = 'FGL%d_BGL%d' % (occlusion_level, occlusion_level)
+        else:
+            self.root_dir = os.path.join(root_dir, 'PASCAL3D_NeMo')
+            self.data_pendix = ''
+
+        self.input_dim = input_dim
+        self.offset = offset
+        self.cls_name = cls_choice
+
+        self.image_path = os.path.join(self.root_dir, 'images')
+
+        if not cls_choice:
+            cls_choice = categories
+
+        self.image_names = [{'category': cat, 'name': name}
+                            for cat in cls_choice
+                            for name in os.listdir(os.path.join(self.image_path, cat + self.data_pendix))
+                            if name not in blacklist[cat]]
+
+        self.annotation_path = os.path.join(self.root_dir, 'annotations')
+        self.im_transform = transforms.Compose([transforms.ToTensor(), normalize])
+
+    def __len__(self):
+        return len(self.image_names)
+
+    def __getitem__(self, idx):
+        img_name = self.image_names[idx]['name']
+        category = self.image_names[idx]['category']
+        cat_index = np.array([categories.index(category)])
+        cat_index = torch.from_numpy(cat_index).long()
+
+        # TODO read bbox instead
+        annotation_file = np.load(os.path.join(
+            self.annotation_path, category + self.data_pendix, img_name.split('.')[0] + '.npz'), allow_pickle=True)
+        upper, lower, left, right, _, _ = annotation_file['box_obj']
+
+        # load gt viewpoint label
+        label = np.array([annotation_file['azimuth'], annotation_file['elevation'], annotation_file['theta']])
+        label *= 180.0 / np.pi
+
+        # load images in RGB format
+        img_path = os.path.join(
+            self.root_dir, 'images', category + self.data_pendix, img_name)
+        im = Image.open(img_path).convert('RGB')
+        im_pos = im.copy()
+
+        if left > right or lower < upper:
+            raise ValueError('Sample %s[%s] has invalid bbox' % (category, img_name))
+
+        # crop the original image with GT box
+        im = im.crop((left, upper, right, lower))
+        im_flip = im.transpose(Image.FLIP_LEFT_RIGHT)
+        im = resize_pad(im, self.input_dim)
+        im_flip = resize_pad(im_flip, self.input_dim)
+        im = self.im_transform(im)
+        im_flip = self.im_transform(im_flip)
+
+        # change angle labels to values >= 0
+        label = process_viewpoint_label(label, self.offset)
+
+        return cat_index, im, label, im_flip
+
+class KITTI3D_NeMo(data.Dataset):
+    def __init__(self, root_dir, input_dim=224, offset=0, occlusion_level=0):
+        assert occlusion_level in [0, 1, 2]
+
+        self.root_dir = os.path.join(root_dir, 'KITTI3D_val_NeMo')
+        self.input_dim = input_dim
+        self.offset = offset
+
+        self.image_path = os.path.join(self.root_dir, 'images')
+        occlusion_levels = {-1: '', 0: '_fully_visible', 1: '_partly_occluded', 2: '_largely_occluded'}
+        list_path = os.path.join(self.root_dir, 'lists', 'car%s' % occlusion_levels[occlusion_level], 'car%s.txt' % occlusion_levels[occlusion_level])
+        self.image_names = [x.strip() for x in open(os.path.join(list_path)).readlines()]
+
+        self.annotation_path = os.path.join(self.root_dir, 'annotations')
+        self.im_transform = transforms.Compose([transforms.ToTensor(), normalize])
+
+    def __len__(self):
+        return len(self.image_names)
+
+    def __getitem__(self, idx):
+        img_name = self.image_names[idx]
+        category = 'car'
+        cat_index = np.array([categories.index(category)])
+        cat_index = torch.from_numpy(cat_index).long()
+
+        # read bbox
+        annotation_file = np.load(os.path.join(
+            self.annotation_path, category, img_name.split('.')[0] + '.npz'), allow_pickle=True)
+        upper, lower, left, right, _, _ = annotation_file['box_obj']
+
+        # load gt viewpoint label
+        label = np.array([annotation_file['azimuth'], annotation_file['elevation'], annotation_file['theta']])
+        label *= 180.0 / np.pi
+
+        # load images in RGB format
+        img_path = os.path.join(self.image_path, category, img_name)
+        im = Image.open(img_path).convert('RGB')
+        im_pos = im.copy()
+
+        if left > right or lower < upper:
+            raise ValueError('Sample %s[%s] has invalid bbox' % ('car', img_name))
+
+        # crop the original image with GT box
+        # im = im.crop((left, upper, right, lower))
+        im_flip = im.transpose(Image.FLIP_LEFT_RIGHT)
+        im = resize_pad(im, self.input_dim)
+        im_flip = resize_pad(im_flip, self.input_dim)
+        im = self.im_transform(im)
+        im_flip = self.im_transform(im_flip)
+
+        # change angle labels to values >= 0
+        label = process_viewpoint_label(label, self.offset)
+
+        return cat_index, im, label, im_flip
 
 class Pascal3D(data.Dataset):
     def __init__(self, root_dir, annotation_file, train=True, input_dim=224, offset=0, shot=None, train_feat=False,
@@ -40,11 +169,11 @@ class Pascal3D(data.Dataset):
         frame = frame[frame.elevation != 90]
         frame = frame[frame.difficult == 0]
         self.cls_names = np.unique(frame.cls_name).tolist()
-        
+
         # align viewpoint annotation in ObjectNet3D to the same format as Pascal3D
         if 'ObjectNet3D' in annotation_file:
             frame.azimuth = (360. + frame.azimuth) % 360
-        
+
         # filter out occluded or truncated samples in evaluation
         if train or train_feat:
             frame = frame[frame.set == 'train']
@@ -168,7 +297,7 @@ class Pascal3D(data.Dataset):
             im_flip = self.im_transform(im_flip)
 
         # chaneg angle labels to values >= 0
-        label = process_viewpoint_label(label, self.offset)
+        # label = process_viewpoint_label(label, self.offset)
 
         if self.train:
             return cls_index, im, label, im_flip, label_flip, im_rot, label_rot, im_pos
@@ -178,12 +307,28 @@ class Pascal3D(data.Dataset):
 
 
 if __name__ == '__main__':
-    import sys
-    root_dir = '/home/xiao/Projects/TransferViewpoint/data/Pascal3D'
-    annotation_file = 'Pascal3D.txt'
+    # cls_choice = []
+    cls_choice = ['car']
 
-    bs = 8
-    trainset = Pascal3D(train=True, root_dir=root_dir, annotation_file=annotation_file, pose_batch=True, bs=bs)
-    dataloader = data.DataLoader(trainset, batch_size=bs, shuffle=False, num_workers=4, drop_last=True)
-    for i, data in enumerate(dataloader):
-        cls_index, im, label, im_flip, label_flip, im_rot, label_rot, im_pos = data
+    mode = 2
+    if mode == 0:
+        bs = 8
+        root_dir = '/media/gkouros/Elements SE/Datasets/pascal3d/PASCAL3D+_release1.1'
+        annotation_file = 'Pascal3D.txt'
+        trainset = Pascal3D(train=False, root_dir=root_dir, annotation_file=annotation_file, pose_batch=True, bs=bs, cls_choice=cls_choice)
+    elif mode == 1:
+        root_dir = '/media/gkouros/Elements SE/Datasets/pascal3d'
+        trainset = Pascal3D_NeMo(root_dir=root_dir, cls_choice=cls_choice)
+    elif mode ==2:
+        root_dir = '/media/gkouros/Elements SE/Datasets/KITTI3D'
+        trainset = KITTI3D_NeMo(root_dir=root_dir, occlusion_level=0)
+
+    idx = np.random.randint(0, len(trainset))
+    cat_index, im, label, im_flip = trainset[idx]
+    print(label)
+    cv2.imshow('', im.detach().permute(1,2,0).cpu().numpy())
+    cv2.waitKey(0)
+
+    # dataloader = data.DataLoader(trainset, batch_size=bs, shuffle=False, num_workers=4, drop_last=True)
+    # for i, data in enumerate(dataloader):
+    #     cls_index, im, label, im_flip, label_flip, im_rot, label_rot, im_pos = data
