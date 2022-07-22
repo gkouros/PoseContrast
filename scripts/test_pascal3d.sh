a7bfc061080cb74c4bced7dd059028188a9f1376
George Kouros <george.kouros.ece@gmail.com>
2022-07-22 11:10:08 +0200

    Add scripts to evaluate on pascal3d and kitti3d

---
+++ scripts/test_pascal3d.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gpu=0
+exp_dir="exps/PoseContrast_Pascal3D_MOCOv2"
+
+python src/test.py --gpu $gpu --dataset Pascal3D --ckpt ${exp_dir}/ckpt.pth --nemo_format --occlusion_level 3
