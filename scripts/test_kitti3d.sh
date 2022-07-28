819dbdcad2edaa0edc56be787766e480b8ad89d1
George Kouros <george.kouros.ece@gmail.com>
2022-07-28 11:46:53 +0200

    Adjust evaluation scripts

--- scripts/test_kitti3d.sh
+++ scripts/test_kitti3d.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 gpu=0
-exp_dir="exps/PoseContrast_Pascal3D_MOCOv2"
+# exp_dir="exps/PoseContrast_Pascal3D_MOCOv2"
+exp_dir="exps/13452.PoseContrast_Pascal3D_NeMo_MOCO_v2"
 
-python src/test.py --gpu $gpu --dataset KITTI3D --ckpt ${exp_dir}/ckpt.pth --nemo_format --occlusion_level 2
+python src/test.py --gpu $gpu --dataset KITTI3D --ckpt ${exp_dir}/ckpt.pth --nemo_format --occlusion_level $1
--- scripts/test_pascal3d.sh
+++ scripts/test_pascal3d.sh
@@ -2,6 +2,11 @@
 
 gpu=0
 # exp_dir="exps/PoseContrast_Pascal3D_MOCOv2"
-exp_dir="exps/PoseContrast_Pascal3D_NeMo_ImageNetk1_v2"
+# exp_dir="exps/PoseContrast_Pascal3D_NeMo_ImageNetk1_v2"
+exp_dir="exps/13452.PoseContrast_Pascal3D_NeMo_MOCO_v2"
+# exp_dir="exps/13456.PoseContrast_Pascal3D_NeMo_MOCO_v2_no_crop/"
+# exp_dir="exps/13455.PoseContrast_Pascal3D_NeMo_ImageNetk1_v2_more_epochs"
 
-python src/test.py --gpu $gpu --dataset Pascal3D --ckpt ${exp_dir}/ckpt.pth --crop --nemo_format --occlusion_level 3
+python src/test.py --gpu $gpu --dataset Pascal3D --ckpt ${exp_dir}/ckpt.pth --crop --nemo_format --occlusion_level $1
+# python src/test.py --gpu $gpu --dataset Pascal3D --ckpt ${exp_dir}/ckpt.pth --nemo_format --occlusion_level $1
+# python src/test.py --gpu $gpu --dataset Pascal3D --ckpt ${exp_dir}/ckpt.pth --crop --occlusion_level $1
